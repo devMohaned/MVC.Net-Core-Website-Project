3c3dcb707170bf4dca3fcd7a57cd31572d818cb1
devMohaned <businessmohaned@gmail.com>
2022-08-31 03:30:41 +0200

    Add project files.

---
+++ Final-Project/wwwroot/js/CategoryList.js
@@ -0,0 +1,12 @@
+﻿let getAllBrands = document.getElementById("getAllBrands");
+let inputGroupSelect01 = document.getElementById("inputGroupSelect01");
+
+getAllBrands.onclick = function () {
+    if (getAllBrands.checked ==true) {
+        inputGroupSelect01.setAttribute("disabled", "disabled");
+    } else {
+        inputGroupSelect01.removeAttribute("disabled");
+
+    }
+    console.log(getAllBrands.checked);
+}
